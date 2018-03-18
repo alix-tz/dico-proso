9629ea73c7b7dc46034ca78033850815d4042339
aureliavasile <aurelia.vasile@gmail.com>
2018-03-18 22:20:18 +0200

    creation route index

--- hoozhoo/routes/generic.py
+++ hoozhoo/routes/generic.py
@@ -9,5 +9,14 @@ def debut():
 
     return "Hello"
 
+@app.route("/index")
+def index():
+    """
+    Route qui affiche la liste des personnes (Nom, prenom) de la base.
+    """
+    personnes = Person.query.order_by(Person.person_id).limit(5).all()
+    return render_template("pages/index.html", personnes=personnes)
+
+
 
 
